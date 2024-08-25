30715fc5168a4db522ddef602289230c4acaa09d
fy622 <yuta.fumoto@gmail.com>
2024-08-25 09:33:45 +0900

    add:generatorの設定追加

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,11 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.skip_routes true
+      g.helper false
+      g.test_framework nil
+    end
   end
 end
